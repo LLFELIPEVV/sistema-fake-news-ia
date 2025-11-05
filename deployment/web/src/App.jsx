801ee668aa303e4767491fd9438afef2537367bd
FELIPE <felipe3133770868@gmail.com>
2025-11-05 13:24:30 -0500

    Pagina de resultado

--- deployment/web/src/App.jsx
+++ deployment/web/src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Index } from "./pages/index";
+import { Resultado } from "./pages/resultado";
 
 function App() {
     return (
         <BrowserRouter>
             <Routes>
                 <Route path="/" element={<Index />} />
+                <Route path="/resultado" element={<Resultado />} />
             </Routes>
         </BrowserRouter>
     );
